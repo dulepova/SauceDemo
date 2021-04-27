89f39fec8df24a8a6f92e8d2d7e8342871dfd32c
Tester <test@mailinator.com>
2021-04-27 11:22:44 +0300

    c

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,14 @@
 package tests;
 
+import org.testng.ITestContext;
+import pages.*;
+import tests.CapabilitiesGenerator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import pages.LoginPage;
-import pages.ProductsPage;
+import org.testng.annotations.Listeners;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,13 +18,15 @@ public class BaseTest {
     ProductsPage productsPage;
 
     @BeforeMethod
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
-        driver = new ChromeDriver();
+    public void setup(ITestContext iTestContext, ITestResult testResult) {
+        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        iTestContext.setAttribute("driver",driver);
+        //       testResult.getTestContext().setAttribute("driver", browser);
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
+        
     }
 
     @AfterMethod(alwaysRun = true)
